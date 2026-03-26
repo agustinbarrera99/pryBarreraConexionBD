d3bba8749918a647e597765a7a8283b3cc0d8eeb
Alumno <Alumno@PC123>
2026-03-26 09:46:35 -0300

    Agregar archivos de proyecto.

---
+++ pryBarreraConexionBD/ClassConexionDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace pryBarreraConexionBD
+{
+    internal class ClassConexionDB
+    {
+        public void ConectarDB()
+        {
+            OleDbConnection conn = new OleDbConnection();
+
+            conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; " +
+                "Data Source=" + Application.StartupPath + "\\..\\..\\base\\baseJuegoRPG.accdb";
+
+            conn.Open();
+
+        }
+
+
+
+    }
+}
---
+++ pryBarreraConexionBD/frmPrincipal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryBarreraConexionBD
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            ClassConexionDB objConectarDB = new ClassConexionDB();
+            try 
+            {
+                objConectarDB.ConectarDB();
+                lblEstadoConexion.Text = "Base de datos conectada";
+                lblEstadoConexion.BackColor = Color.Green;
+            } catch {
+                lblEstadoConexion.Text = "Error al conectar la base de datos";
+                lblEstadoConexion.BackColor = Color.Red;
+            }
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
